0914f532051dde96d69089e90733dbfcdebd0d46
Ben Harrison <ben@newagainhouses.com>
2023-11-27 17:07:20 -0500

    adding AnnualRent example

---
+++ CalculationEngine/AnnualRent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationEngine;
+public class AnnualRent
+{
+    public decimal? Calculate(decimal? monthlyRent)
+    {
+        if (monthlyRent == null || monthlyRent < 0)
+            return null;
+
+        return 12 * monthlyRent;
+    }
+}
---
+++ CalculationEngineUnitTests/AnnualRentTests.cs
@@ -0,0 +1,38 @@
+﻿using CalculationEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculationEngineUnitTests;
+
+[TestClass]
+public class AnnualRentTests
+{
+
+    [TestMethod]
+    public void AnnualRent_Calculate__500month()
+    {
+        var obj = new AnnualRent();
+        var result = obj.Calculate(500);
+        Assert.AreEqual(6000, result);
+    }
+
+    [TestMethod]
+    public void AnnualRent_Calculate__null()
+    {
+        var obj = new AnnualRent();
+        var result = obj.Calculate(null);
+        Assert.IsNull(result);
+    }
+
+    [TestMethod]
+    public void AnnualRent_Calculate__negative1()
+    {
+        var obj = new AnnualRent();
+        var result = obj.Calculate(-1);
+        Assert.IsNull(result);
+    }
+
+}
